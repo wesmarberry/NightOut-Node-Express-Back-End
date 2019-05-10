06e1901bf7cc789f66f582a719e5c8e1202a1245
Wes <wesmarberry@gmail.com>
2019-05-09 21:08:40 -0500

    activity create and show route working

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -6,6 +6,43 @@ const Activity = require('../models/activity');
 
 
 
+//create route
+
+router.post('/', async (req, res, next) => {
+	try {
+		const createdActivity = await Activity.create(req.body)
+		res.json({
+			status: 201,
+			data: createdActivity,
+			session: req.session
+		})
+
+
+
+	} catch (err) {
+		next(err)
+	}
+})
+
+//show route
+
+router.get('/:id', async (req, res, next) => {
+	try {
+		const foundActivity = await User.findById(req.params.id)
+		res.json({
+			status: 200,
+			data: foundActivity,
+			session: req.session
+		})
+
+
+
+	} catch (err) {
+		next(err)
+	}
+})
+
+
 
 
 
